749ab23837e11fe557b94170c71ac34275505057
rupesh <rupeshpatel2207@gmail.com>
2024-09-06 12:34:00 +0530

    Added notification feature in the admin part

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express')
-const { loginController, registerController, authController } = require('../controllers/userCtrl')
+const { loginController, 
+    registerController, 
+    authController,
+    applyDoctorController,
+ } = require('../controllers/userCtrl')
 const authMiddleware = require('../middlewares/authMiddleware')
 
 //router object
@@ -14,4 +18,7 @@ router.post('/register', registerController)
 
 //Auth || post
 router.post('/getUserData', authMiddleware, authController);
-module.exports = router
+
+//APply Doctor || POST
+router.post("/apply-doctor", authMiddleware, applyDoctorController);
+module.exports = router;
